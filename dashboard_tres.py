cd63bb2200fedb76035b23cc2f3752a2e31ca3ce
KarlaIvo <a1281841@uabc.edu.mx>
2024-12-03 18:04:20 -0800

    dashboard 3 cambios 2

--- dashboard_tres.py
+++ dashboard_tres.py
@@ -5,74 +5,73 @@ import dash_bootstrap_components as dbc
 
 data = pd.read_csv("assets/datasets/Clean_bc_collabs.csv")
 
-def grafica_histograma(data):
-    fig_histograma = px.histogram(data, y='Price', nbins=20, title='Distribución de Precios de Colaboraciones', orientation='h')
-    fig_histograma.update_traces(marker=dict(color="#7fb3d5"))
-    fig_histograma.update_layout(
+def grafica_hist(data):
+    fig_hist = px.histogram(data, y='Price', nbins=20, title='Distribución de Precios de Colaboraciones', orientation='h')
+    fig_hist.update_traces(marker=dict(color="#7fb3d5"))
+    fig_hist.update_layout(
         paper_bgcolor="#fadbd8",
         plot_bgcolor="#fadbd8",
         title=dict(font=dict(size=20), x=0.5)
     )
-    return fig_histograma
+    return fig_hist
 
-def grafica_top_caros():
-    top_caros = data.nlargest(10, 'Price')
-    fig_barras_caros = px.bar(top_caros, x='Title', y='Price', title='Top 10 Colaboraciones más Caras')
-    fig_barras_caros.update_traces(marker=dict(color="#a93226"))
-    fig_barras_caros.update_layout(
+@callback(
+    Output("figDistPriceColb", "figure"),
+    Input("figDistPriceColb", "id")
+)
+def act_hist(_):
+    fig_hist = grafica_hist(data)
+    return fig_hist
+
+def grafica_caros():
+    caros = data.nlargest(10, 'Price')
+    fig_barCaros = px.bar(caros, x='Title', y='Price', title='Top 10 Colaboraciones más Caras')
+    fig_barCaros.update_traces(marker=dict(color="#a93226"))
+    fig_barCaros.update_layout(
         paper_bgcolor="#fadbd8",
         plot_bgcolor="#fadbd8",
         title=dict(font=dict(size=20), x=0.5)
     )
-    return fig_barras_caros
+    return fig_barCaros
 
-def grafica_top_baratos():
-    top_baratos = data.nsmallest(10, 'Price')
-    fig_barras_baratos = px.bar(top_baratos, x='Title', y='Price', title='Top 10 Colaboraciones más Baratas')
-    fig_barras_baratos.update_traces(marker=dict(color="#45b39d"))
-    fig_barras_baratos.update_layout(
+@callback(
+    Output("figColbCaros", "figure"),
+    Input("figColbCaros", "id")
+)
+def act_caros(_):
+    fig_barCaros = grafica_caros()
+    return fig_barCaros
+
+def grafica_baratos():
+    baratos = data.nsmallest(10, 'Price')
+    fig_barBaratos = px.bar(baratos, x='Title', y='Price', title='Top 10 Colaboraciones más Baratas')
+    fig_barBaratos.update_traces(marker=dict(color="#45b39d"))
+    fig_barBaratos.update_layout(
         paper_bgcolor="#fadbd8",
         plot_bgcolor="#fadbd8",
         title=dict(font=dict(size=20), x=0.5)
     )
-    return fig_barras_baratos
+    return fig_barBaratos
 
+@callback(
+    Output("figColbBaratos", "figure"),
+    Input("figColbBaratos", "id")
+)
+def act_baratos(_):
+    fig_barBaratos = grafica_baratos()
+    return fig_barBaratos
 def dashboard():
     body = html.Div([
         dbc.Row(html.Div([html.H3("Distribución de precios y 10 colaboraciones más caras", style={"color": "white", "text-align": "center", "margin-top": "30px"})]), className="row_uno"),
         dbc.Row([
-            dbc.Col(dcc.Graph(id="figDistribucionPreciosColb")),
-            dbc.Col(dcc.Graph(id="figTopColbCaros")),
-            dbc.Col(dcc.Graph(id="figTopColbBaratos")),
+            dbc.Col(dcc.Graph(id="figDistPriceColb")),
+            dbc.Col(dcc.Graph(id="figColbCaros")),
+            dbc.Col(dcc.Graph(id="figColbBaratos")),
         ])
     ])
     return body
 
 
-@callback(
-    Output("figDistribucionPreciosColb", "figure"),
-    Input("figDistribucionPreciosColb", "id")
-)
-def update_histograma(_):
-    fig_histograma = grafica_histograma(data)
-    return fig_histograma
-
-@callback(
-    Output("figTopColbCaros", "figure"),
-    Input("figTopColbCaros", "id")
-)
-def update_top_caros(_):
-    fig_barras_caros = grafica_top_caros()
-    return fig_barras_caros
-
-
-@callback(
-    Output("figTopColbBaratos", "figure"),
-    Input("figTopColbBaratos", "id")
-)
-def update_top_baratos(_):
-    fig_barras_baratos = grafica_top_baratos()
-    return fig_barras_baratos
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
